4e1cb3689fcef216bf90847b616ee8b3f58fa58e
molpopgen <krthornt@uci.edu>
2022-02-26 12:07:59 -0800

    fix (I think...) the lsp not in path warnings

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -78,7 +78,7 @@ local opts = {
     },
 }
 
-require('rust-tools').setup(opts)
+-- require('rust-tools').setup(opts)
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -87,13 +87,16 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    -- on_attach = my_custom_on_attach,
-    capabilities = capabilities,
-  }
-end
+-- local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+-- for _, lsp in ipairs(servers) do
+--   -- require('lspconfig')[lsp].setup {
+--   --   capabilities = capabilities
+--   -- }
+--   -- lspconfig[lsp].setup {
+--   --   -- on_attach = my_custom_on_attach,
+--   --   capabilities = capabilities,
+--   -- }
+-- end
 
 -- luasnip setup
 local luasnip = require 'luasnip'
