f0161612ed3019679a7c29a2d773cf9079c3eaa9
molpopgen <krthornt@uci.edu>
2022-06-16 10:08:36 -0700

    move diagnostics commands to keybindings.lua

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -5,6 +5,16 @@ local function nkeymap(key, map)
     keymap('n', key, map, opts)
 end
 
+-- From https://github.com/neovim/nvim-lspconfig
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
 nkeymap('gd', ':lua vim.lsp.buf.definition()<cr>')
 nkeymap('gD', ':lua vim.lsp.buf.declaration()<cr>')
 nkeymap('gi', ':lua vim.lsp.buf.implementation()<cr>')
