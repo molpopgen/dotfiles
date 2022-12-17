dfa99e7264794cc856b628a29a67acc205e2ec64
Kevin R. Thornton <krthornt@uci.edu>
2022-12-17 13:46:50 -0800

    treesitter textobjects for nvim

--- nvim_refactor/lua/krt/packer.lua
+++ nvim_refactor/lua/krt/packer.lua
@@ -21,6 +21,7 @@ require('packer').startup(function(use)
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
     use { 'nvim-treesitter/nvim-treesitter-context' }
+    use { 'nvim-treesitter/nvim-treesitter-textobjects' }
 
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
