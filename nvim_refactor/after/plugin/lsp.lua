1f17b696c65c7cbdc8d31fe17e3537c811e35163
Kevin R. Thornton <krthornt@uci.edu>
2025-09-26 10:13:36 -0700

    prevent lsp from starting up twice

--- nvim_refactor/after/plugin/lsp.lua
+++ nvim_refactor/after/plugin/lsp.lua
@@ -20,7 +20,10 @@ end
 
 require("mason").setup()
 require("mason-lspconfig").setup(
-    { ensure_installed = { 'clangd', 'lua_ls', 'pylsp', 'r_language_server', 'rust_analyzer' ,'tinymist'} }
+    {
+        automatic_enable = false,
+        ensure_installed = { 'clangd', 'lua_ls', 'pylsp', 'r_language_server', 'rust_analyzer', 'tinymist' }
+    }
 )
 
 require("lspconfig").lua_ls.setup {}
@@ -38,9 +41,9 @@ require("lspconfig").pylsp.setup({
         },
     },
 })
-require'lspconfig'.tinymist.setup{
-	settings = {
-	}
+require 'lspconfig'.tinymist.setup {
+    settings = {
+    }
 }
 
 set_lsp_keymappings()
